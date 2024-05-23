54a594c605c22b2f64d64749e1be39d0b3e1be03
DareenSuryadi <dareen.422023008@civitas.ukrida.ac.id>
2024-05-23 21:03:18 +0700

    .

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,11 +2,28 @@
 
 namespace App\Models;
 
-// use Illuminate\Illuminate\Eloquent\Factories\HasFactory;
+// use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\User;
+use OpenApi\Annotations as OA;
 
+/**
+ * Class Book.
+ * 
+ * @author  Dareen <dareen.422023008@civitas.ukrida.ac.id>
+ * 
+ * @OA\Schema(
+ *     description="Book model",
+ *     title="Book model",
+ *     required={"title", "author"},
+ *     @OA\Xml(
+ *         name="Book"
+ *     )
+ * )
+ */
+
+ 
 class Book extends Model
 {
     // use HasFactory;
